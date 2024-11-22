9516606f96f83a0aa08fd610f7f9c6020a0f883b
BagasArdiansaPutra <bagasardiansa06.com>
2024-11-22 10:24:31 +0700

    jumat berkah

--- Kafe06.java
+++ Kafe06.java
@@ -1,13 +1,21 @@
 public class Kafe06 {
     public static void main(String[] args) {
-        Menu ("Andi", true);
+        Menu("Andi", true, "DISKON50"); 
     }
-        public static void Menu(String namaPelanggan, boolean isMember) {
-            System.out.println("Selamat datang, " + namaPelanggan + "!");
 
-            if (isMember) {
-                System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-            }
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Selamat! Anda mendapatkan diskon 50% dengan kode promo: DISKON50");
+        if (kodePromo.equals("DISKON30")) {
+        }
+            System.out.println("Anda mendapatkan diskon 30% dengan kode promo: DISKON30");
+        } else {
+            System.out.println("Kode promo tidak valid.");
+        }
         System.out.println("=== MENU RESTO KAFE ===");
         System.out.println("1. Kopi Hitam - Rp. 15,000");
         System.out.println("2. Cappucino - Rp. 20,000");
@@ -17,5 +25,5 @@ public class Kafe06 {
         System.out.println("6. Mie Goreng - Rp. 18,000");
         System.out.println("=======================");
         System.out.println("Silahkan pilih menu yang anda inginkan");
-}
+    }
 }
