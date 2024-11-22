6cf84e01ed901c2593edd3407add5d6380b6c3dc
BagasArdiansaPutra <bagasardiansa06.com>
2024-11-22 10:15:05 +0700

    jumat berkah

--- Kafe06.java
+++ Kafe06.java
@@ -1,5 +1,13 @@
 public class Kafe06 {
     public static void main(String[] args) {
+        Menu ("Andi", true);
+    }
+        public static void Menu(String namaPelanggan, boolean isMember) {
+            System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+            if (isMember) {
+                System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+            }
         System.out.println("=== MENU RESTO KAFE ===");
         System.out.println("1. Kopi Hitam - Rp. 15,000");
         System.out.println("2. Cappucino - Rp. 20,000");
@@ -9,30 +17,5 @@ public class Kafe06 {
         System.out.println("6. Mie Goreng - Rp. 18,000");
         System.out.println("=======================");
         System.out.println("Silahkan pilih menu yang anda inginkan");
- 
-        int pilihan = 1; 
-        
-        switch (pilihan) {
-            case 1:
-                System.out.println("Anda memilih Kopi Hitam - Rp. 15,000");
-                break;
-            case 2:
-                System.out.println("Anda memilih Cappucino - Rp. 20,000");
-                break;
-            case 3:
-                System.out.println("Anda memilih Latte - Rp. 22,000");
-                break;
-            case 4:
-                System.out.println("Anda memilih Teh Tarik - Rp. 12,000");
-                break;
-            case 5:
-                System.out.println("Anda memilih Roti Bakar - Rp. 10,000");
-                break;
-            case 6:
-                System.out.println("Anda memilih Mie Goreng - Rp. 18,000");
-                break;
-            default:
-                System.out.println("Pilihan tidak valid.");
-        }
-    }
+}
 }
